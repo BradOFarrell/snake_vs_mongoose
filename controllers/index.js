37db9e4ffe2cbec505090504646702352f3f18df
BradOFarrell <bradofarrell@gmail.com>
2017-09-29 14:45:22 -0400

    Successfully pulled random users and user path data from the server.

--- controllers/index.js
+++ controllers/index.js
@@ -6,18 +6,21 @@ const UserModel = Schema.UserModel;
 
 router.get('/', (request, response) => {
   UserModel.find({})
-        .then((user) => {
-            // Get random opponent from ghost data
-            const opponent = user[Math.floor(Math.random()*user.length)];
-            console.log(opponent);
-            response.render('index', {
-              opponent: opponent
-            })
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-});
+      .then((user) => {
 
+        // Get random opponent from ghost data
+          const opponent = user[Math.floor(Math.random()*user.length)];
+          const mongoosePath = opponent.logs[Math.floor(Math.random()*opponent.logs.length)].log;
+          console.log(opponent);
+
+          response.render('index', {
+            opponentName: opponent.username,
+            opponentLog: mongoosePath
+          })
+      })
+      .catch((error) => {
+          console.log(error)
+      })
+});
 
 module.exports = router;
