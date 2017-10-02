b78a587d78247fce96ad7347ccbdc0911d423ff6
BradOFarrell <bradofarrell@gmail.com>
2017-10-01 21:03:07 -0400

    Big commit! Got inheretence working on objects, got game state and HTML view to instantiate, got main function and loop function to work; next up, fiinally doing the actual game logic.

--- public/js/snake.js
+++ public/js/snake.js
@@ -5,74 +5,134 @@
 
 // Stores main variables for game
 const State = {
-    board: [[],[]],
-    boardSize: 5, 
     playerName: "Brad",
-    opponentName: opponentName
+    opponentName: opponentName,
+    boardSize: 5, 
+    snake: {},
+    mongoose: {},
+    board: new Array(this.boardSize),
+    initializeData: ()=> {
+        for(let i = 0; i < State.boardSize; i++){
+            State.board[i] = new Array(State.boardSize);
+            for(let j = 0; j < State.boardSize; j++){
+                State.board[i][j] = new Grass(i,j);
+                console.log("Created: "+i+","+j+" which contains "+State.board[i][j].img+"!");
+            }
+        }
+
+        State.snake = new Snake(1,1)        
+        State.board[1][1] = State.snake;
+        console.log("Assigned snake to 1, 1!");
+        
+        State.mongoose = new Mongoose(State.boardSize-1,State.boardSize-2)
+        State.board[State.boardSize-1][State.boardSize-2] = State.mongoose;
+        console.log("Assigned mongoose to "+(State.boardSize-1)+","+(State.boardSize-2)+"!");
+        
+        State.board[2][2] = new Apple(2,2);
+    }
 }
 
 // Updates the HTML based on game state
 const View = {
-    createStage: ()=> {
+    initializeHTML: ()=> {
         let outputHTML = ""
         for(let i = 0; i < State.boardSize; i++){
             for(let j = 0; j < State.boardSize; j++){
-                outputHTML+="<img src='/img/grass.png' id='x"+i+"y"+j+"'>";
+                outputHTML+="<img src='/img/"+State.board[i][j].img+".png' id='x"+i+"y"+j+"'>";
             }
             outputHTML+="</br>";            
         }
         $('#gameBoard').html(outputHTML);
 //        View.updateImage(2,3,"apple");
 //        View.updateImage(0,0,"snake_body");
-        console.log(outputHTML);
+        console.log("Created HTML Block: "+outputHTML);
     },
-    updateImage: (x,y,imgName)=> {
-        $("#x"+x+"y"+y).attr("src","img/"+imgName+".png");
-        console.log("#x"+x+"y"+y+" set to img/"+imgName+".png");
+    updateImage: (x,y,img)=> {
+        $("#x"+x+"y"+y).attr("src","img/"+img+".png");
+        console.log("#x"+x+"y"+y+" set to img/"+img+".png");
     }
 }
 
-class Body {
-    constructor(x,y,timer){
+class Sprite {
+    constructor(x,y){
         this.x = x;
         this.y = y;
-        this.timer = timer;
+        this.img = "apple";
+    }
+    onEachTurn(turn){
+        console.log("["+this.x+","+this.y+"] Sprite: "+this.img)
+    }
+    updateSprite(imgName){
+        this.img = imgName;
+        View.updateImage(this.x,this.y,imgName);
     }
 }
 
-class Apple {
-    constructor(x,y,timer){
-        this.x = x;
-        this.y = y;
+class Grass extends Sprite {
+    constructor(x,y){
+        super(x,y);
+        this.img = "grass";
+    }
+    onEachTurn(turn){   
+        console.log("Grasssssss tastes bad!");
+    }
+}
+
+class Body extends Sprite {
+    constructor(x,y,timer,img){
+        super(x,y);
         this.timer = timer;
+        this.img = img;
+    }
+}
+
+class Apple extends Sprite {
+    constructor(x,y){
+        super(x,y);
+        this.img = "apple";
+        this.timer = 5;
+    }
+    onEachTurn(turn){
+        this.timer--;
+        if(this.timer < 3){
+            this.img = "apple_"+this.timer;
+        } else if(this.timer == 0){
+            State.board[this.x][this.y] = new Grass(this.x,this.y);
+        }
     }
 }
 
 // HAndles opponent-related data
-const Mongoose = {
-    
+class Mongoose extends Sprite {
+    constructor(x,y){
+        super(x,y);
+        this.direction = "n";
+        this.lastMove = "n";
+        this.img ="mongoose_head_n"
+    }
 }
 
 // Handles player-related data
-const Snake = {
-    direction: "s",
-    lastMove: "s",
-    x: 0,
-    y: 0,
-    turn: (d)=>{
-        // Prevent head from moving backwards or forwards
-        if(!((Snake.lastMove == "s" && d == "n")
-           ||(Snake.lastMove == "n" && d == "s")
-           ||(Snake.lastMove == "e" && d == "w")
-           ||(Snake.lastMove == "w" && d == "e"))
-           && (Snake.lastMove != d)) {
-            // Update movement direction
-            Snake.direction = d;
-            Snake.lastMove = d;
-            View.updateImage(Snake.x,Snake.y,"snake_head_"+d);
-            View.updateImage(2,1,"snake_head_w");    
-         }
+class Snake extends Sprite {
+    constructor(x,y){
+        super(x,y);
+        this.direction = "s";
+        this.lastMove = "s";
+        this.img ="snake_head_s"
     }
+    rotate(d){
+        // Prevent head from moving backwards or forwards
+        if(!((this.lastMove == "s" && d == "n")
+           ||(this.lastMove == "n" && d == "s")
+           ||(this.lastMove == "e" && d == "w")
+           ||(this.lastMove == "w" && d == "e"))
+           && (this.lastMove != d)) {
+            // Update movement direction
+            this.direction = d;
+            this.lastMove = d;
+            View.updateImage(this.x,this.y,"snake_head_"+d);
+         }
+    }   
 }
 
 // Capture player input, send it to the Snake object
@@ -82,16 +142,16 @@ $(document).keydown((e)=> {
     }    
     switch(e.key) {
     case "ArrowUp":
-        Snake.turn("n");
+        State.snake.rotate("n");
         break;
     case "ArrowDown":
-        Snake.turn("s");
+        State.snake.rotate("s");
         break;
     case "ArrowLeft":
-        Snake.turn("w");
+        State.snake.rotate("w");
         break;
     case "ArrowRight":
-        Snake.turn("e");
+        State.snake.rotate("e");
         break;
     default:
         return; // exit this handler for other keys
@@ -99,8 +159,22 @@ $(document).keydown((e)=> {
     e.preventDefault(); // prevent the default action (scroll / move caret)
 });
 
+// Real time gameplay loop
+function loop(interval, turn){
+    turn++;
+    for(let i = 0; i < State.boardSize; i++){
+        for(let j = 0; j < State.boardSize; j++){
+            State.board[i][j].onEachTurn(0);
+        }
+    }
+    console.log(turn);
+    setTimeout(()=>{loop(interval, turn)},interval);
+}
+
 // Run initial functionality when page is fully loaded
 function main(){
-    View.createStage();    
+    State.initializeData();
+    View.initializeHTML();
+    loop(1000,0);
 }
 $(document).attr("onload",main());
