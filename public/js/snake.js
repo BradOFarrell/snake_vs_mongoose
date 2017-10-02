efb6cfcb0f3e58590c6ff517fdeb108630ae3bc0
BradOFarrell <bradofarrell@gmail.com>
2017-10-02 03:27:15 -0400

    Got baseline snake movement working. Calling it a night, will resume tomorrow and: Complete snake movement; complete apple functionality; begin mongoose movement

--- public/js/snake.js
+++ public/js/snake.js
@@ -7,7 +7,9 @@
 const State = {
     playerName: "Brad",
     opponentName: opponentName,
-    boardSize: 5, 
+    mongoosePath: mongoosePath,
+    results: results,
+    boardSize: 6, 
     snake: {},
     mongoose: {},
     board: new Array(this.boardSize),
@@ -20,9 +22,9 @@ const State = {
             }
         }
 
-        State.snake = new Snake(1,1)        
-        State.board[1][1] = State.snake;
-        console.log("Assigned snake to 1, 1!");
+        State.snake = new Snake(0,1)        
+        State.board[0][1] = State.snake;
+        console.log("Assigned snake to 0, 1!");
         
         State.mongoose = new Mongoose(State.boardSize-1,State.boardSize-2)
         State.board[State.boardSize-1][State.boardSize-2] = State.mongoose;
@@ -35,19 +37,23 @@ const State = {
 // Updates the HTML based on game state
 const View = {
     initializeHTML: ()=> {
-        let outputHTML = ""
+        let outputHTML = "";
+        let styling = "light";
         for(let i = 0; i < State.boardSize; i++){
             for(let j = 0; j < State.boardSize; j++){
-                outputHTML+="<img src='/img/"+State.board[i][j].img+".png' id='x"+i+"y"+j+"'>";
+                if(j%2 == i%2){
+                    styling = "dark";
+                } else {
+                    styling = "light";
+                }
+                outputHTML+="<img src='/img/"+State.board[i][j].img+".png' id='x"+i+"y"+j+"' class='"+styling+"'>";
             }
             outputHTML+="</br>";            
         }
         $('#gameBoard').html(outputHTML);
-//        View.updateImage(2,3,"apple");
-//        View.updateImage(0,0,"snake_body");
         console.log("Created HTML Block: "+outputHTML);
     },
-    updateImage: (x,y,img)=> {
+    changeSrc: (x,y,img)=> {
         $("#x"+x+"y"+y).attr("src","img/"+img+".png");
         console.log("#x"+x+"y"+y+" set to img/"+img+".png");
     }
@@ -60,11 +66,13 @@ class Sprite {
         this.img = "apple";
     }
     onEachTurn(turn){
-        console.log("["+this.x+","+this.y+"] Sprite: "+this.img)
     }
-    updateSprite(imgName){
-        this.img = imgName;
-        View.updateImage(this.x,this.y,imgName);
+    updateSprite(){
+        View.changeSrc(this.x,this.y,this.img);
+    }
+    replaceWith(Obj){
+        State.board[this.x][this.y] = Obj;
+        State.board[this.x][this.y].updateSprite();
     }
 }
 
@@ -73,17 +81,7 @@ class Grass extends Sprite {
         super(x,y);
         this.img = "grass";
     }
-    onEachTurn(turn){   
-        console.log("Grasssssss tastes bad!");
-    }
-}
 
-class Body extends Sprite {
-    constructor(x,y,timer,img){
-        super(x,y);
-        this.timer = timer;
-        this.img = img;
-    }
 }
 
 class Apple extends Sprite {
@@ -94,10 +92,80 @@ class Apple extends Sprite {
     }
     onEachTurn(turn){
         this.timer--;
-        if(this.timer < 3){
+        if(this.timer < 4 && this.timer > 0){
             this.img = "apple_"+this.timer;
+            this.updateSprite();
         } else if(this.timer == 0){
-            State.board[this.x][this.y] = new Grass(this.x,this.y);
+            this.replaceWith(new Grass(this.x, this.y));
+        }
+    }
+}
+
+// Handles player-related data
+class Snake extends Sprite {
+    constructor(x,y){
+        super(x,y);
+        this.currentlyFacing = "s";
+        this.lastMove = "s";
+        this.lastTurnMoved = 0;
+        this.img ="snake_head_s"
+    }
+    rotate(direction){
+        // Prevent head from moving backwards or forwards
+        if(!((this.lastMove == "s" && direction == "n")
+           ||(this.lastMove == "n" && direction == "s")
+           ||(this.lastMove == "e" && direction == "w")
+           ||(this.lastMove == "w" && direction == "e"))
+           && (this.lastMove != direction)) {
+            // Update movement direction
+            this.currentlyFacing = direction;
+            this.lastMove = direction;
+            this.img = "snake_head_"+direction;
+            this.updateSprite();
+         }
+    }
+    onEachTurn(turn){
+        console.log(this.lastTurnMoved+" "+turn);
+        if(this.lastTurnMoved < turn){
+            // Turn old panel into body panel
+            let oldX = this.x;
+            let oldY = this.y;
+            let newBody = new SnakeBody(oldX,oldY);
+            State.board[oldX][oldY] = newBody;
+            newBody.updateSprite();
+
+            // Change position based on directional input
+            if(this.currentlyFacing == "n")
+                this.x--;
+            if(this.currentlyFacing == "s")
+                this.x++;
+            if(this.currentlyFacing == "e")
+                this.y++;
+            if(this.currentlyFacing == "w")
+                this.y--;
+
+            // Update position on the board
+            State.board[this.x+1][this.y] = this
+            this.updateSprite();
+            this.lastTurnMoved = turn;
+        }
+    }
+}
+
+// Snake body placed on tiles when the snake moves, references State.snake
+class SnakeBody extends Sprite {
+    constructor(x,y){
+        super(x,y);
+        this.timer = 2;
+        this.img = "snake_body"
+    }
+    onEachTurn(turn){
+        this.timer--;
+        if(this.timer == 1){
+        this.img = "snake_tail"
+        this.updateSprite();
+        } else if(this.timer == 0){
+            this.replaceWith(new Grass(this.x, this.y));
         }
     }
 }
@@ -112,29 +180,12 @@ class Mongoose extends Sprite {
     }
 }
 
-// Handles player-related data
-class Snake extends Sprite {
+class MongooseBody extends Sprite {
     constructor(x,y){
         super(x,y);
-        this.direction = "s";
-        this.lastMove = "s";
-        this.img ="snake_head_s"
+        this.timer = timer;
     }
-    rotate(d){
-        // Prevent head from moving backwards or forwards
-        if(!((this.lastMove == "s" && d == "n")
-           ||(this.lastMove == "n" && d == "s")
-           ||(this.lastMove == "e" && d == "w")
-           ||(this.lastMove == "w" && d == "e"))
-           && (this.lastMove != d)) {
-            // Update movement direction
-            this.direction = d;
-            this.lastMove = d;
-            View.updateImage(this.x,this.y,"snake_head_"+d);
-         }
-    }   
 }
-
 // Capture player input, send it to the Snake object
 $(document).keydown((e)=> {
     if (e.defaultPrevented) {
@@ -164,7 +215,7 @@ function loop(interval, turn){
     turn++;
     for(let i = 0; i < State.boardSize; i++){
         for(let j = 0; j < State.boardSize; j++){
-            State.board[i][j].onEachTurn(0);
+            State.board[i][j].onEachTurn(turn);
         }
     }
     console.log(turn);
